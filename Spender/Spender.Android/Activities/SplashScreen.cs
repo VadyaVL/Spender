5b8e0ec774b56ed0b6323cddc4b37c10cc92889c
Vadym Lytvyn <vadyavl@gmail.com>
2018-03-03 21:11:49 +0200

    Added splash as drawable resource
    Find way to use lottie via layer

--- Spender/Spender.Android/Activities/SplashScreen.cs
+++ Spender/Spender.Android/Activities/SplashScreen.cs
@@ -3,12 +3,14 @@ using Android.OS;
 
 namespace Spender.Droid
 {
+    // Use splash via theme or SetContentView (find solution)
     [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashScreen : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             this.SetContentView(Resource.Layout.Splash);
 
             this.StartActivity(typeof(MainActivity));
